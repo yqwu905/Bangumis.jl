8624b42657e7f63174d79e0d2bc4acb268236313
pingline <pingline@outlook.com>
2022-08-28 10:52:27 +0800

    test(Utils): Add more test case for date_parse.

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,6 @@ using Dates: DateTime
     @test Bangumis.Utils.date_parse("2022-02") == DateTime(2022, 02, 01)
     @test Bangumis.Utils.date_parse("2022") == DateTime(2022, 01, 01)
     @test Bangumis.Utils.date_parse("31.07.2022") == DateTime(2022, 07, 31)
-    @test Bangumis.Utils.date_parse("2022-02-01") == DateTime(2022, 02, 01)
-    @test Bangumis.Utils.date_parse("2022-02-01") == DateTime(2022, 02, 01)
+    @test Bangumis.Utils.date_parse("fdjkalsjfdklas") == DateTime(1970)
+    @test Bangumis.Utils.date_parse("0") == DateTime(0)
 end
