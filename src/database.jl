ef014d1a7c26035d4d0dc3aec2233aa5eb4b5305
pingline <pingline@outlook.com>
2022-08-28 21:04:23 +0800

    feat(database): Implement database table structure check.

--- src/database.jl
+++ src/database.jl
@@ -5,6 +5,22 @@ using ..Bangumis: config
 
 export prepare_db
 
+function verify_db_table(db::SQLite.DB, tbl_name::AbstractString, tbl_cols::DataFrame)::Bool
+    cols = DataFrame(SQLite.columns(db, tbl_name))
+    if (size(cols)!=size(tbl_cols))
+        return false
+    end
+    for i in 1:size(cols, 1)
+        for j in 1:size(cols, 2)
+            if (cols[i, j]!==tbl_cols[i, j])
+                @error "Database $db mismatch require columns at ($i, $j): $(cols[i, j]) is given, $(tbl_cols[i, j]) is required."
+                return false
+            end
+        end
+    end
+    return true
+end
+
 function prepare_db(filename::Union{AbstractString, Nothing}=nothing)::SQLite.DB
     if (isnothing(filename))
         filename = config["index"]["db"]
