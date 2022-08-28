f574cab157367e51f90359ed11f4cf9ad857b344
pingline <pingline@outlook.com>
2022-08-28 21:04:52 +0800

    feat(database): Add SUBJECTS table structure dataframe.

--- src/database.jl
+++ src/database.jl
@@ -5,6 +5,16 @@ using ..Bangumis: config
 
 export prepare_db
 
+# Table Definition
+const SUBJECTS_TBL_DF = DataFrame(
+    cid = collect(0:12),
+    name = ["id","url","type","name","name_cn","summary","air_date","air_weekday","image_large","image_common","image_medium","image_small","image_grid"],
+    type = ["INTEGER","TEXT","INTEGER","TEXT","TEXT","TEXT","TEXT","INTEGER","TEXT","TEXT","TEXT","TEXT","TEXT"],
+    notnull = [1,1,1,1,0,0,1,1,0,0,0,0,0],
+    dflt_value = ones(Missing, 13),
+    pk = [1,0,0,0,0,0,0,0,0,0,0,0,0]
+)
+
 function verify_db_table(db::SQLite.DB, tbl_name::AbstractString, tbl_cols::DataFrame)::Bool
     cols = DataFrame(SQLite.columns(db, tbl_name))
     if (size(cols)!=size(tbl_cols))
