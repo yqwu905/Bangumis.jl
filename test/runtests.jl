125564448cb43b2ff3f3b68cc8b59f90766f777a
pingline <pingline@outlook.com>
2022-08-30 19:31:31 +0800

    test(Utils): Add test for missing_eq.

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,12 +8,20 @@ Logging.global_logger(Logging.ConsoleLogger(stdout, Logging.Debug))
 cd(dirname(dirname(pathof(Bangumis))))
 
 @testset "Utils" begin
+    # date_parse
     @test Bangumis.Utils.date_parse("2022-02-01") == DateTime(2022, 02, 01)
     @test Bangumis.Utils.date_parse("2022-02") == DateTime(2022, 02, 01)
     @test Bangumis.Utils.date_parse("2022") == DateTime(2022, 01, 01)
     @test Bangumis.Utils.date_parse("31.07.2022") == DateTime(2022, 07, 31)
     @test Bangumis.Utils.date_parse("fdjkalsjfdklas") == DateTime(1970)
     @test Bangumis.Utils.date_parse("0") == DateTime(0)
+    # missing_eq
+    @test Bangumis.Utils.missing_eq(missing, missing)
+    @test Bangumis.Utils.missing_eq(1, 1.0)
+    @test Bangumis.Utils.missing_eq(Int64(1), Int32(1))
+    @test !Bangumis.Utils.missing_eq(missing, 1)
+    @test !Bangumis.Utils.missing_eq(2, 1)
+    @test !Bangumis.Utils.missing_eq(1, DateTime(1970))
 end
 
 @testset "Config" begin
