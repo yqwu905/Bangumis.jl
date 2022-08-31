a7f4a747bf6f89e2e4c65fc94944e12024deedc6
pingline <pingline@outlook.com>
2022-08-31 11:07:16 +0800

    feat(base): Add struct Subject and Episode.

--- src/Bangumis.jl
+++ src/Bangumis.jl
@@ -4,6 +4,32 @@ using TOML: parsefile
 
 export config
 
+struct Subject
+    id::Integer
+    url::AbstractString
+    type::Int
+    name::AbstractString
+    name_cn::AbstractString
+    summary::AbstractString
+    air_date::DateTime
+    air_weekday::Integer
+    images::NamedTuple{(:large, :common, :medium, :small, :grid), Tuple{AbstractString, AbstractString, AbstractString, AbstractString, AbstractString}}
+end
+
+struct Episode
+    id::Integer
+    type::Integer
+    name::AbstractString
+    name_cn::AbstractString
+    sort::Integer
+    ep::Integer
+    air_date::DateTime
+    comment::AbstractString
+    duration::AbstractString
+    desc::AbstractString
+    disc::AbstractString
+end
+
 const DEFAULT_CONFIG_FILE = joinpath(
     dirname(dirname(pathof(@__MODULE__))), "data", "config.toml")
 config = parsefile(DEFAULT_CONFIG_FILE)
