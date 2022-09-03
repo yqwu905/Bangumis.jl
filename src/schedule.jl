1a131e5c3aca4374cf556ebd7de8d92f90ca5caf
pingline <pingline@outlook.com>
2022-09-04 00:52:57 +0800

    feat(schedule): Add callback function.

--- src/schedule.jl
+++ src/schedule.jl
@@ -10,33 +10,34 @@ struct Job
     f::Function
     params::Tuple
     err::Integer
+    callback::Function
 end
 
 struct Result
     id::Integer
     success::Bool
     res::Any
+    callback::Function
 end
 
-Base.show(io::IO, j::Job) = print(io, "Job $(j.id) $(String(Symbol(j.f)))($(join(j.params, ", ")))")
+Base.show(io::IO, j::Job) = print(io, "Job $(j.id) $(String(Symbol(j.f)))($(join(j.params, ", "))), Callback: $(String(Symbol(j.callback)))")
 
-Job(id::Integer, f::Function, params::Tuple) = Job(id, f, params, 0)
-Job(id::Integer, f::Function) = Job(id, f, ())
-create_jobs_pool(size::Integer = 1000) = Channel{Job}(size)
+Job(id::Integer, f::Function, params::Tuple, callback::Function) = Job(id, f, params, 0, callback)
+Job(id::Integer, f::Function, callback::Function) = Job(id, f, (), 0, callback)
+create_jobs_pool(size::Integer = 1000) = (Channel{Job}(size), Channel{Result}(size))
 
 function job_executator(pool::Channel{Job}, res::Channel{Result}, max_retries::Integer = 5)
     for job in pool
         try
             r = job.f(job.params...)
-            put!(res, Result(job.id, true, r))
+            put!(res, Result(job.id, true, r, job.callback))
         catch e
-            max_retries = config["base"]["max_retries"]
             if (job.err < max_retries)
                 @warn "$job failed: $e, Retry $(job.err)/$max_retries"
-                put!(pool, Job(job.id, job.f, job.params, job.err + 1))
+                put!(pool, Job(job.id, job.f, job.params, job.err + 1, job.callback))
             else
                 @error "$job failed: $e, max retries exceed, stop executing."
-                put!(res, Result(job.id, false, missing))
+                put!(res, Result(job.id, false, missing, job.callback))
             end
         end
     end
