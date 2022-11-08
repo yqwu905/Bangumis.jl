2e4629cd7280b711884e73fdd043b96151b8eb4d
yqwu905 <pingline@outlook.com>
2022-11-08 10:02:08 +0800

    format(BT): Format file and add some comment.

--- src/BT/bencode.jl
+++ src/BT/bencode.jl
@@ -4,12 +4,13 @@ using DataStructures
 using ...Bangumis.Utils: dynamic_parse_int
 
 # Type Rename
-const BString = Vector{UInt8};
-const BInt = Integer;
-const BList = Vector;
-const BStr = String;
-const BDict = Dict;
-const BObject = Union{BStr, BInt, BList, BDict};
+const BByteStr = Vector{UInt8}
+const BInt = Integer
+const BList = Vector
+const BStr = String
+const BDict = OrderedDict
+const BObject = Union{BStr,BInt,BList,BDict,BByteStr}
+
 
 """
     bdecode(data::Vector{UInt8})::BObject
@@ -23,29 +24,29 @@ julia> bdecode(Vector{UInt8}("i32e")
 ```
 """
 function bdecode(data::Vector{UInt8})::BObject
-    first_delim = popfirst!(data);
-
+    first_delim = popfirst!(data)
     # Integer decode
     if (first_delim == 0x69)
-        buf = UInt8[];
-        delim = popfirst!(data);
+        buf = UInt8[]
+        delim = popfirst!(data)
         while (delim != 0x65)
-            push!(buf, delim);
-            delim = popfirst!(data);
+            push!(buf, delim)
+            delim = popfirst!(data)
         end
         return dynamic_parse_int(String(buf))
-    # String decode
+        # String decode
     elseif (first_delim >= 0x31 && first_delim <= 0x39)
-        len_buf = UInt8[];
-        delim = first_delim;
+        len_buf = UInt8[]
+        delim = first_delim
         while (delim != 0x3a)
-            push!(len_buf, delim);
-            delim = popfirst!(data);
+            push!(len_buf, delim)
+            delim = popfirst!(data)
         end
         len = dynamic_parse_int(String(len_buf))
         buf = [popfirst!(data) for _ in 1:len]
+        # return buf
         return String(buf)
-    # List decode
+        # List decode
     elseif (first_delim == 0x6c)
         list = BObject[]
         while (data[1] != 0x65)
@@ -53,7 +54,7 @@ function bdecode(data::Vector{UInt8})::BObject
         end
         popfirst!(data)
         return list
-    # Dict decode
+        # Dict decode
     elseif (first_delim == 0x64)
         dict = BDict{BObject,BObject}()
         iskey = true
