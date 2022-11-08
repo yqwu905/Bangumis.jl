944245fc1673bd564d5edc87870b24b79c2e5e2c
yqwu905 <pingline@outlook.com>
2022-11-08 10:03:31 +0800

    feat(BT): Implement function to generate info-hash from torrent file.

--- src/BT/bittorrent.jl
+++ src/BT/bittorrent.jl
@@ -1,5 +1,16 @@
 module BitTorrent
 
+using SHA
+
 include("bencode.jl")
+
+function get_info_hash(torrent_file::AbstractString)::Vector{UInt8}
+    info = BEncode.bdecode(read(torrent_file))["info"]
+    if (haskey(info, "pieces"))
+        pieces = info["pieces"]
+        info["pieces"] = Vector{UInt8}(pieces)
+    end
+    return BEncode.bencode(info) |> sha1
+end
 
 end
