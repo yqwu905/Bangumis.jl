6996ce8a1fc3b0686b59361f21ffdb7e13381f9b
yqwu905 <pingline@outlook.com>
2022-11-10 21:45:13 +0800

    refactor(Schedule): Move schedule related function from Bangumis.jl to Schedule.jl
    * Function `daemon`, `start_main_thread` and `close_main_thread` move into schedule.jl.
    * Declaration of `pool` `res` and `dump_res` are move into schedule.jl.
    * Usage in test are modified.

--- test/schedule.jl
+++ test/schedule.jl
@@ -2,18 +2,18 @@ println("##################################")
 println("#      Test for Schedule         #")
 println("##################################")
 
-start_main_thread()
+Bangumis.Schedule.start_main_thread()
 
 cases = 1:1000
 for i in cases
     j1 = Job(i, Bangumis.Test.addone)
     j = Job(i, Bangumis.Test.power, (i,), j1)
-    push!(Bangumis.pool, j)
+    push!(Bangumis.Schedule.pool, j)
 end
 expected_output = map(x->x^2+1, cases)
 output = Vector{Union{Missing, Integer}}(missing, length(cases))
 for i in cases
-    res = take!(Bangumis.dump_res)
+    res = take!(Bangumis.Schedule.dump_res)
     output[res.id] = res.res
 end
 
